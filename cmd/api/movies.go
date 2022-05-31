f719dc5301382f99b5f749145b94b4276cae4267
Aleksey Kuzmin <aleschakuzm1n@yandex.ru>
2022-05-31 15:16:10 +0300

    Добавленя оптимистичные блокировки для частичного и полного обновления записей

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -76,29 +76,14 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 }
 
 func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := app.readIDParam(r)
-	if err != nil {
-		app.notFoundResponse(w, r)
-		return
-	}
-	movie, err := app.models.Movies.Get(id)
-	if err != nil {
-		switch {
-		case errors.Is(err, data.ErrRecordNotFound):
-			app.notFoundResponse(w, r)
-		default:
-			app.serverErrorResponse(w, r, err)
-		}
-		return
-	}
-
+	movie := app.readAndGetMovie(w, r)
 	var input struct {
 		Title   string       `json:"title"`
 		Year    int32        `json:"year"`
 		Runtime data.Runtime `json:"runtime"`
 		Genres  []string     `json:"genres"`
 	}
-	err = app.readJSON(w, r, &input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
@@ -107,20 +92,36 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	movie.Year = input.Year
 	movie.Runtime = input.Runtime
 	movie.Genres = input.Genres
-	v := validator.New()
-	if data.ValidateMovie(v, movie); !v.Valid() {
-		app.failedValidationResponse(w, r, v.Errors)
+	app.validateAndUpdate(movie, w, r)
+}
+
+func (app *application) partialUpdateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	movie := app.readAndGetMovie(w, r)
+
+	var input struct {
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
 		return
 	}
-	err = app.models.Movies.Update(movie)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
+	if input.Title != nil {
+		movie.Title = *input.Title
 	}
-	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
+	if input.Year != nil {
+		movie.Year = *input.Year
 	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
+	app.validateAndUpdate(movie, w, r)
 }
 
 func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -140,6 +141,47 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 	err = app.writeJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) readAndGetMovie(w http.ResponseWriter, r *http.Request) *data.Movie {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return nil
+	}
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return nil
+	}
+	return movie
+}
+
+func (app *application) validateAndUpdate(movie *data.Movie, w http.ResponseWriter, r *http.Request) {
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	err := app.models.Movies.Update(movie)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
